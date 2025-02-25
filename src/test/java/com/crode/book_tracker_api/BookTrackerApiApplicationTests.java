88049dd6c8190d55c876d41a4e04090bb04a8e7c
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-02-25 09:45:16 +0200

    feat: Initialize Spring Boot project with PostgreSQL and essential dependencies.

---
+++ src/test/java/com/crode/book_tracker_api/BookTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crode.book_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
